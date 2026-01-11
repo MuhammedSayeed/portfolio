54a10e142bc1badf8504a134ef135c4744524c31
Mohamed Sayed <mhmdsayed90031@gmail.com>
2026-01-11 08:19:01 +0200

    feat(ui): implement navbar component

--- components/navbar/contact-me.tsx
+++ components/navbar/contact-me.tsx
@@ -1,7 +1,13 @@
+import { ArrowRight } from "lucide-react"
 
 const ContactMe = () => {
     return (
-        <div className="flex justify-end">ContactMe</div>
+        <div className="relative flex items-center gap-2 text-3xl uppercase group cursor-pointer h-10 overflow-hidden">
+            <span>Contact Me</span>
+            <ArrowRight className="size-8 -rotate-45 group-hover:rotate-0 transition-transform duration-300" />
+            <span className="absolute bottom-[2px] z-10 w-full bg-white h-[2px] group-hover:translate-x-full transition-all duration-300 delay-100 group-hover:delay-0" />
+            <span className="absolute bottom-[2px] -translate-x-full z-10 w-full bg-white h-[2px] group-hover:translate-x-0 transition-all duration-300 group-hover:delay-100" />
+        </div>
     )
 }
 
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,14 +1,20 @@
 import Logo from "@/components/logo"
 import ContactMe from "@/components/navbar/contact-me"
-import NavItems from "@/components/navbar/nav-items"
+import MenuButton from "@/components/navbar/menu-button"
+import NavItemsLargeScreen from "@/components/navbar/nav-items-large-screen"
 
 
 const Navbar = () => {
     return (
-        <nav className="py-9 px-16 flex items-center justify-between sticky bg-amber-900 z-100 top-0 left-0 right-0">
+        <nav className="py-9 px-6 md:px-16 flex items-center justify-between sticky z-50 top-0 left-0 right-0 text-white bg-transparent mix-blend-difference">
             <Logo />
-            <NavItems />
-            <ContactMe />
+            <div className="hidden xl:flex justify-between w-full">
+                <NavItemsLargeScreen />
+                <ContactMe />
+            </div>
+            <div className="xl:hidden">
+                <MenuButton />
+            </div>
         </nav>
     )
 }
