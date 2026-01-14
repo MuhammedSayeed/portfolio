b1db9e26f836812e9bcdd17cd3bdfc97ec47974f
Mohamed Sayed <mhmdsayed90031@gmail.com>
2026-01-14 12:49:53 +0200

    apply code reviews

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -6,7 +6,7 @@ import NavItemsLargeScreen from "@/components/navbar/nav-items-large-screen"
 
 const Navbar = () => {
     return (
-        <nav className="py-9 px-6 md:px-16 flex items-center justify-between sticky z-50 top-0 left-0 right-0 text-white bg-transparent mix-blend-difference">
+        <nav className="py-9 px-6 md:px-16 flex items-center justify-between fixed z-50 top-0 bg-transparent left-0 right-0 text-white  mix-blend-difference">
             <Logo />
             <div className="hidden xl:flex justify-between w-full">
                 <NavItemsLargeScreen />
